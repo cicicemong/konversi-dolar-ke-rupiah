e84fe4795165240077c6102dbaf123e770e31af5
cici cemong <45526554+cicicemong@users.noreply.github.com>
2019-01-13 09:16:22 +0700

    Add files via upload

---
+++ KONVERSI DOLAR KE RUPIAH.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int main()
+{
+    //deklarasi
+    int d,rp;
+    //deskripsi
+    cout<<"mata uang dolar ="<<endl;
+    cin>>d;
+    //hkonversi
+    rp=d*15100;
+    cout<<"uang rupiah ="<<rp<<endl;
+    system ("pause");
+    return 0;
+}
